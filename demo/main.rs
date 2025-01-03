fafd4f6ec84c47ec0d82cbe74a084e0d556e0202
SpiderUnderUrBed <SpiderUnderUrBed@proton.me>
2025-01-03 17:09:55 +1300

    latest

--- demo/main.rs
+++ demo/main.rs
@@ -1,172 +1,67 @@
-use iced::application::Title;
-use iced::widget::container;
-use iced::{Application, Color, Element, Settings, Subscription, Theme};
-use iced_grid::{Grid, RowData, CellMessage};
+use grid::{Cell, Factory, Grid};
+use iced::{
+    Background, Color, Element, Length,
+    advanced::widget::Text,
+    run,
+    widget::{Container, container::Style},
+};
+use itertools::Itertools;
 
-#[derive(Debug, Clone)]
-enum Message {
-    Ui(UiMessage),
-    Grid(iced_grid::GridMessage),
+struct State<'a> {
+    grid: Grid<'a, Message>,
 }
 
-#[derive(Debug, Clone)]
-enum UiMessage {
-    AddRow,
-    AddCell(usize), // usize represents the row to which a cell will be added
-    ButtonClicked(usize, usize),
-    Sync,
-}
-
-impl From<UiMessage> for Message {
-    fn from(ui_message: UiMessage) -> Self {
-        Message::Ui(ui_message)
-    }
-}
-
-impl From<iced_grid::GridMessage> for Message {
-    fn from(grid_message: iced_grid::GridMessage) -> Self {
-        Message::Grid(grid_message)
-    }
-}
-
-pub struct MyApp {
-    grid: Grid<Message, MyTheme>,
-}
-
-use iced::{Background};
-
-#[derive(Debug, Clone)]
-pub struct MyStyle {
-    pub background_color: Color,
-    pub text_color: Color,
-    pub padding: f32,
-}
-
-
-impl Default for MyApp {
+impl Default for State<'_> {
     fn default() -> Self {
-        let rows = vec![];
-
-        // Create the grid
-        let mut grid = Grid::new(
-            rows,
-            container::Style {
-                background: Some(Background::Color(Color::BLACK)),
-                ..Default::default()
-            },
-            |_offset: iced::widget::scrollable::AbsoluteOffset| UiMessage::Sync.into(),
-        );
-
-        // Add an initial row to the grid
-        let mut row = RowData::default();
-        row.push_text("Row 1, Cell 1".into());
-        row.push_button("Add Row".into(), CellMessage::Clicked);
-        row.push_button("Add Cell".into(), CellMessage::Clicked);
-        row.push_container(container("test"));
-        grid.add_row(row);
-        grid.add_cells_to_all_rows(5);
-        grid.style(
-            container::Style {
-                background: Some(Background::Color(Color::BLACK)),
-                ..Default::default()
-            }
-        );
-        MyApp { grid }
-    }
-}
-
-#[derive(Clone)]
-pub struct MyTheme;
-
-impl iced_grid::style::Catalog for MyTheme {
-    type Style = container::Style;
-
-    fn body(&self, _style: &Self::Style) -> iced::widget::container::Style {
-        container::Style {
-            background: Some(iced::Background::Color(Color::from_rgb(0.8, 0.8, 0.8))),
-            ..Default::default()
-        }
-    }
-
-    fn cell(&self, _row: usize, _col: usize) -> iced::widget::container::Style {
-        container::Style {
-            background: Some(iced::Background::Color(Color::from_rgb(0.6, 0.6, 0.9))),
-            ..Default::default()
-        }
-    }
-}
-
-impl MyApp {
-<<<<<<< HEAD:demo/centered/main.rs
-    fn view<'a>(&'a mut self) -> iced::Element<'a, Message> {
-        let centered_grid= Column::new()
-            .push(Space::with_height(Length::Fill)) 
-            .push(
-                Row::new()
-                    .push(Space::with_width(Length::Fill)) 
-                    .push(self.grid.view()) 
-                    .push(Space::with_width(Length::Fill)), 
+        const DAYS_PER_WEEK: usize = 7;
+        let today = 10;
+        let grid = Grid::new()
+            .with_row(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"])
+            .with_rows(
+                (1..=31)
+                    .map(move |day| {
+                        Factory::from_factory(move || {
+                            let red = day == today;
+                            Cell::from(Text::new(day)).style(if red {
+                                Style {
+                                    background: Some(Background::Color(Color::from_rgb8(
+                                        255, 0, 0,
+                                    ))),
+                                    text_color: Some(Color::from_rgb8(255, 255, 255)),
+                                    ..Style::default()
+                                }
+                            } else {
+                                Style {
+                                    background: Some(Background::Color(Color::from_rgb8(
+                                        255, 255, 255,
+                                    ))),
+                                    text_color: Some(Color::from_rgb8(0, 0, 0)),
+                                    ..Style::default()
+                                }
+                            })
+                        })
+                    })
+                    .chunks(DAYS_PER_WEEK)
+                    .into_iter()
+                    .map(Itertools::collect_vec)
+                    .collect_vec(),
             )
-            .push(Space::with_height(Length::Fill));
-
-        let element: Element<'_, Message> = Element::new(centered_grid).map(Message::from);
-        element
-=======
-    fn view<'a>(&'a self) -> iced::Element<'a, Message> {
-        self.grid.view().map(Message::from)
->>>>>>> parent of 8cb6ef8 (latest):demo/main.rs
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Ui(ui_message) => match ui_message {
-                UiMessage::AddRow => {
-                    let mut new_row = RowData::default();
-                    let row_index = self.grid.row_count();
-                    new_row.push_text(format!("Row {}, Cell 1", row_index + 1).into());
-                    new_row.push_button("Add Row".into(), CellMessage::Clicked);
-                    new_row.push_button("Add Cell".into(), CellMessage::Clicked);
-                    self.grid.add_row(new_row);
-                }
-                UiMessage::AddCell(row_index) => {
-                    if let Some(row) = self.grid.get_row_mut(row_index) {
-                        let cell_count = row.cells.len() - 2; // Exclude Add Row and Add Cell buttons
-                        row.push_text(format!("Row {}, Cell {}", row_index + 1, cell_count + 1).into());
-                    }
-                }
-                UiMessage::ButtonClicked(row, col) => {
-                   // grid.add_cells_to_all_rows(5);
-                    println!("Button clicked in row {}, column {}", row, col);
-                }
-                UiMessage::Sync => {
-                    println!("Syncing...");
-                }
-            },
-            Message::Grid(grid_message) => match grid_message {
-                iced_grid::GridMessage::Cell(row, col, CellMessage::Clicked) => {
-                    // Determine action based on the column index
-                    if col == 1 {
-                        // Add Row button clicked
-                        self.update(Message::Ui(UiMessage::AddRow));
-                    } else if col == 2 {
-                        // Add Cell button clicked
-                        self.update(Message::Ui(UiMessage::AddCell(row)));
-                    }
-                }
-                _ => {
-                    // Handle other grid messages if necessary
-                    println!("Grid message received: {:?}", grid_message);
-                }
-            },
-        }
-    }
-    
-
-    fn theme(&self) -> Theme {
-        Theme::default()
+            .cell_height(50)
+            .cell_width(50)
+            .padding(5);
+        Self { grid }
     }
 }
 
-fn main() -> iced::Result {
-    iced::run("main", MyApp::update, MyApp::view)
+#[derive(Debug, Clone, Copy)]
+enum Message {}
+
+fn main() {
+    run("grid", update, view).unwrap();
+}
+
+fn update(_state: &mut State, _message: Message) {}
+
+fn view<'a>(state: &'a State) -> Element<'a, Message> {
+    Container::new(&state.grid).center(Length::Fill).into()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,252 +1,188 @@
+//! # grid
+//! A thin wrapper around `iced`'s `Column` widget to create a grid of cells.
+#![warn(missing_docs, clippy::pedantic, clippy::nursery)]
 use iced::{
-    widget::{Button, Text},
-    Element, Theme,
-};
-
-use iced_widget::{
-    container, scrollable, Column, Row 
-
-};
-
-use std::sync::Arc;
-use std::cell::RefCell;
-
-use style::wrapper::{Target, Wrapper};
-pub use style::Catalog;
-pub mod style;
-
-#[derive(Debug, Clone)]
-pub enum CellMessage {
-    Edit,
-    Remove,
-    Clicked,
-}
-
-#[derive(Debug, Clone)]
-pub enum GridMessage {
-    AddCell(usize),
-    Cell(usize, usize, CellMessage),
-}
-
-pub enum Cell<'a> {
-    Text(String),
-    Button {
-        label: String,
-        on_press: CellMessage,
+    Element, Pixels, Theme, advanced,
+    widget::{
+        Column, Container, Row,
+        container::{self, Style, StyleFn},
     },
-    Container(Arc<RefCell<dyn Fn() -> Element<'a, CellMessage> + 'a>>),
-}
+};
 
-impl<'a> Clone for Cell<'a> {
-    fn clone(&self) -> Self {
-        match self {
-            Cell::Text(content) => Cell::Text(content.clone()),
-            Cell::Button { label, on_press } => Cell::Button {
-                label: label.clone(),
-                on_press: on_press.clone(),
-            },
-            Cell::Container(container) => {
-                Cell::Container(Arc::clone(container))
-            }
-        }
+/// A cell in a grid, storing information about the inner element and the style of the cell.
+pub struct Cell<'a, M, T, R>(Element<'a, M, T, R>, Style);
+
+impl<'a, M, T, R, E: Into<Element<'a, M, T, R>> + 'a> From<E> for Cell<'a, M, T, R> {
+    /// Create a new grid cell with an element in it. The style of the grid cell is set to the default style. See
+    /// the implementation of [`Default`] for [`Style`] for more information.
+    fn from(element: E) -> Self {
+        Self(element.into(), Style::default())
     }
 }
 
-impl Cell<'_> {
-    pub fn view(&self) -> Element<CellMessage> {
-        let element = match self {
-            Cell::Text(content) => Text::new(content.clone()).into(),
-            Cell::Button { label, on_press } => {
-                Button::new(Text::new(label.clone()))
-                    .on_press(on_press.clone())
-                    .into()
-            }
-            Cell::Container(factory) => (factory.borrow())(),
-        };
-        element
+impl<M, T, R> Cell<'_, M, T, R> {
+    /// Set the style of the cell.
+    #[must_use]
+    pub fn style(mut self, style: impl Into<Style>) -> Self {
+        self.1 = style.into();
+        self
     }
 }
 
-#[derive(Default, Clone)]
-pub struct RowData {
-    pub cells: Vec<Cell<'static>>,
+/// A factory for creating cells in a grid.
+pub struct Factory<'a, M, T, R>(Box<dyn Fn() -> Cell<'a, M, T, R> + 'a>);
+
+impl<'a, M, T, R> Factory<'a, M, T, R> {
+    /// Create a new factory that creates a grid cell with an element in it, with the default style.
+    /// See the implementation of [`Default`] for [`Style`] for more information.
+    ///
+    /// The [`Clone`] constraint is necessary because the factory creates owned elements for `iced`
+    /// to consume on each `view` cycle.
+    pub fn from_element<E: Into<Element<'a, M, T, R>> + Clone + 'a>(element: E) -> Self {
+        Self(Box::new(move || {
+            Cell(element.clone().into(), Style::default())
+        }))
+    }
+
+    /// Create a new factory that creates a grid cell with an element in it, with the given style.
+    /// The [`Clone`] constraint is necessary because the factory creates owned elements for `iced`
+    /// to consume on each `view` cycle.
+    pub fn from_element_and_style<E: Into<Element<'a, M, T, R>> + Clone + 'a>(
+        element: E,
+        style: Style,
+    ) -> Self {
+        Self(Box::new(move || Cell(element.clone().into(), style)))
+    }
+
+    /// Create a new factory from the given function.
+    pub fn from_factory<F: Fn() -> Cell<'a, M, T, R> + 'a>(factory: F) -> Self {
+        Self(Box::new(factory))
+    }
 }
 
-impl RowData {
-    pub fn push_text(&mut self, content: String) {
-        self.cells.push(Cell::Text(content));
-    }
-
-    pub fn push_button(&mut self, label: String, on_press: CellMessage) {
-        self.cells.push(Cell::Button { label, on_press });
-    }
-
-    pub fn push_container<F>(&mut self, factory: F)
-    where
-        F: Fn() -> Element<'static, CellMessage> + 'static,
-    {
-        self.cells.push(Cell::Container(Arc::new(RefCell::new(factory))));
-    }
-
-    pub fn len(&self) -> usize {
-        self.cells.len()
-    }
-
-    pub fn get_mut(&mut self, index: usize) -> Option<&mut Cell<'static>> {
-        self.cells.get_mut(index)
+impl<'a, M, T, R, E: Into<Element<'a, M, T, R>> + Clone + 'a> From<E> for Factory<'a, M, T, R> {
+    /// Convert an element into a factory that creates a grid cell with the element in it.
+    /// See [`Self::from_element`] for more information.
+    fn from(element: E) -> Self {
+        Self::from_element(element)
     }
 }
 
-   
-#[derive(Clone)]
-pub struct Grid<Message, Theme>
-where
-    Theme: style::Catalog,
-{
-    rows: Vec<RowData>,
-    style: <Theme as style::Catalog>::Style,
-    on_sync: fn(scrollable::AbsoluteOffset) -> Message,
+/// A grid of cells.
+pub struct Grid<'a, M: 'a, T: 'a = Theme, R: advanced::Renderer + 'a = iced::Renderer> {
+    rows: Vec<Vec<Factory<'a, M, T, R>>>,
+    cell_width: Pixels,
+    cell_height: Pixels,
+    gutter: Pixels,
+    padding: Pixels,
 }
 
-impl<'a, Message, Theme, Renderer> From<Grid<Message, Theme>>
-for Element<'a, Message, Theme, Renderer>
-where
-   
-    Renderer: iced_core::Renderer + 'a,
-    Theme: style::Catalog + container::Catalog + scrollable::Catalog + 'a,
-    Message: 'a + Clone,
-{
-    fn from(grid: Grid<Message, Theme>) -> Self {
-        let style = grid.style.clone(); 
-    
-        Element::new(Wrapper { 
-            content: grid.into(),
-            target: Target::Style,
-            style,
-        })
-    }    
-}
-
-impl<Message, Theme> From<iced::Element<'_, Message, Theme>> for Grid<Message, Theme>
-where
-    Theme: style::Catalog,
-{
-    fn from(_element: iced::Element<'_, Message, Theme>) -> Self {
-        
-        
-        Grid {
+impl<'a, M: 'a, T: 'a, R: advanced::Renderer + 'a> Default for Grid<'a, M, T, R> {
+    /// Create a new grid with no rows, and zero width, height, gutter, and padding.
+    fn default() -> Self {
+        Self {
             rows: Vec::new(),
-            style: Default::default(), 
-            on_sync: |_| panic!("Conversion from Element not implemented"),
+            cell_width: Pixels::default(),
+            cell_height: Pixels::default(),
+            gutter: Pixels::default(),
+            padding: Pixels::default(),
         }
     }
 }
 
-impl<'a, Message, Theme: style::Catalog> Grid<Message, Theme> {
-    pub fn new(rows: Vec<RowData>, style: <Theme as style::Catalog>::Style, on_sync: fn(scrollable::AbsoluteOffset) -> Message ) -> Self {
-        Self { rows, style, on_sync }
-    }
-    
-    pub fn style(&mut self, style: impl Into<<Theme as style::Catalog>::Style>) {
-        self.style = style.into();
-    }
-    
-
-    pub fn get_cell(&mut self, row_index: usize, cell_index: usize) -> Option<&mut Cell<'static>> {
-        self.rows.get_mut(row_index).and_then(|row| row.get_mut(cell_index))
-    }
-
-    pub fn get_row(&mut self, row: usize) -> &mut RowData {
-        if self.rows.len() <= row {
-            self.rows.resize_with(row + 1, RowData::default);
-        }
-        &mut self.rows[row]
-    }
-
-    pub fn row_count(&self) -> usize {
-        self.rows.len()
-    }
-
-    pub fn add_row(&mut self, row: RowData) {
-        self.rows.push(row);
-    }
-
-    pub fn get_row_mut(&mut self, row: usize) -> Option<&mut RowData> {
-        if row < self.rows.len() {
-            Some(&mut self.rows[row])
-        } else {
-            None
-        }
-    }
-    pub fn add_rows(&mut self, count: usize) {
-        for _ in 0..count {
-            self.rows.push(RowData::default());
-        }
-    }
-
-    
-    
-    pub fn add_cells_to_row(&mut self, row_index: usize, count: usize) {
-        let row = self.get_row(row_index); 
-        for _ in 0..count {
-            row.cells.push(Cell::Text("Default".to_string())); 
-        }
-    }
-
-    
-    pub fn add_cells_to_all_rows(&mut self, count: usize) {
-        for row in &mut self.rows {
-            for _ in 0..count {
-                row.cells.push(Cell::Text("Default".to_string())); 
-            }
-        }
-    }
-
-
-    pub fn create_grid(&'a self) -> Column<'a, GridMessage> {
-        let mut column = Column::new().spacing(10);
-
-        for row_index in 0..self.rows.len() {
-            let mut row_view = Row::new().spacing(10);
-            for cell_index in 0..self.rows[row_index].cells.len() {
-                let cell = &self.rows[row_index].cells[cell_index];
-                let cell_view: Element<GridMessage> = match cell {
-                    Cell::Text(ref text) => {
-                        Text::new(text.clone()).into()
-                    }
-                    Cell::Button { ref label, on_press } => {
-                        Button::new(Text::new(label.clone()))
-                            .on_press(GridMessage::Cell(row_index, cell_index, on_press.clone()))
-                            .into()
-                    }
-                    Cell::Container(factory) => (factory.borrow())().map(move |cell_msg| {
-                        GridMessage::Cell(row_index, cell_index, cell_msg)
-                    }),
-                };
-                row_view = row_view.push(cell_view);
-            }
-            column = column.push(row_view);
-        }
-
-        column
-    }
-
-    pub fn view(&'a self) -> iced::Element<'a, GridMessage> 
-    where iced_widget::container::Style: From<<Theme as style::Catalog>::Style>
-    {
-        
-        container(
-            self.create_grid()
-                .padding(10) 
-                .spacing(5), 
+impl<'a, M: 'a, T: 'a + container::Catalog, R: advanced::Renderer + 'a> From<&Grid<'a, M, T, R>>
+    for Element<'a, M, T, R>
+where
+    T::Class<'a>: From<StyleFn<'a, T>>,
+{
+    fn from(
+        Grid {
+            rows,
+            cell_width,
+            cell_height,
+            gutter,
+            padding,
+        }: &Grid<'a, M, T, R>,
+    ) -> Self {
+        Container::new(
+            rows.iter()
+                .map(|row| {
+                    row.iter()
+                        .map(|column| {
+                            let Cell(element, style) = column.0();
+                            Container::new(element)
+                                .center_x(*cell_width)
+                                .center_y(*cell_height)
+                                .style(move |_| style)
+                                .into()
+                        })
+                        .collect::<Row<M, T, R>>()
+                        .spacing(*gutter)
+                        .into()
+                })
+                .collect::<Column<M, T, R>>()
+                .spacing(*gutter),
         )
-        .style({
-            move |theme: &crate::Theme| self.style.clone().into() 
-        })
- 
-        
-        
+        .padding(padding.0)
         .into()
     }
+}
 
+impl<'a, M: 'a, T: 'a, R: advanced::Renderer + 'a> Grid<'a, M, T, R> {
+    /// Create a new grid with no rows, and zero width, height, gutter, and padding.
+    #[must_use]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Add a row to the grid.
+    #[must_use]
+    pub fn with_row<C: Into<Factory<'a, M, T, R>>>(
+        mut self,
+        row: impl IntoIterator<Item = C>,
+    ) -> Self {
+        self.rows.push(row.into_iter().map(Into::into).collect());
+        self
+    }
+
+    /// Add multiple rows to the grid.
+    #[must_use]
+    pub fn with_rows<B: IntoIterator<Item = C>, C: Into<Factory<'a, M, T, R>>>(
+        mut self,
+        rows: impl IntoIterator<Item = B>,
+    ) -> Self {
+        self.rows.extend(
+            rows.into_iter()
+                .map(|row| row.into_iter().map(Into::into).collect()),
+        );
+        self
+    }
+
+    /// Set the width of each cell in the grid.
+    #[must_use]
+    pub fn cell_width(mut self, cell_width: impl Into<Pixels>) -> Self {
+        self.cell_width = cell_width.into();
+        self
+    }
+
+    /// Set the height of each cell in the grid.
+    #[must_use]
+    pub fn cell_height(mut self, cell_height: impl Into<Pixels>) -> Self {
+        self.cell_height = cell_height.into();
+        self
+    }
+
+    /// Set the size of the gutter between rows and columns in the grid.
+    #[must_use]
+    pub fn gutter(mut self, gutter: impl Into<Pixels>) -> Self {
+        self.gutter = gutter.into();
+        self
+    }
+
+    /// Set the padding around the grid.
+    #[must_use]
+    pub fn padding(mut self, padding: impl Into<Pixels>) -> Self {
+        self.padding = padding.into();
+        self
+    }
 }
